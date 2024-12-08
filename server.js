5d74298af8c972b074ad515698d51025dffaab1b
dav1lex <zedxurl@gmail.com>
2024-12-08 12:13:12 +0100

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const db = process.env.DATABASE_URL || require('./database');
+const db = process.env.DATABASE_URL || require('./backend/database');
 require('dotenv').config();
 
 const app = express();
