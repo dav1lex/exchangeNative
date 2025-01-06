af6d2c1bc9aa3356e6100f2bb1ac642c52380626
dav1lex <zedxurl@gmail.com>
2025-01-06 12:23:52 +0100

    api update

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://exchangenative.onrender.com';
 
 // User registration
 export const registerUser = async (email, password) => {
